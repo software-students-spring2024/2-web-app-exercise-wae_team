e1ecfdd58a3171ee3b7021dd31fb311b6c975331
ethanki <144947809+ethanki@users.noreply.github.com>
2024-02-27 22:19:27 -0500

    changed the app.py code and connected the html documents to flask

--- app.py
+++ app.py
@@ -3,9 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/classes')
+def classes():
+    return render_template('classes.html')
+
+@app.route('/classDetails')
+def classDetails():
+    return render_template('classDetails.html')
+
+@app.route('/addClass')
+def addClass():
+    return render_template('addClass.html')
+
+@app.route('/addAssignment')
+def addAssignment():
+    return render_template('addAssignment.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
